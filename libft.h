4e9a010d280c6dcce09a5ebfd76e40ca39417547
Nassr El Yamani <nel-yama@fu-r10-p8.s19.be>
2025-04-22 21:51:31 +0200

    adding function ft_lstdelone

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nel-yama <nel-yama@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 21:06:21 by nel-yama          #+#    #+#             */
-/*   Updated: 2025/04/22 21:16:55 by nel-yama         ###   ########.fr       */
+/*   Updated: 2025/04/22 21:41:10 by nel-yama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
 
 #endif
