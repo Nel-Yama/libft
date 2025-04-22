8d90703024439c8dc75e8378e81252f8414cffa2
Nassr El Yamani <nel-yama@fu-r10-p8.s19.be>
2025-04-22 21:17:58 +0200

    using ft_calloc instead of calloc

--- ft_substr.c
+++ ft_substr.c
@@ -31,7 +31,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	slen = ft_strlen(s);
 	if (start >= slen)
-		return (calloc(1, 1));
+		return (ft_calloc(1, 1));
 	if (len > slen - start)
 		len = slen - start;
 	subs = malloc((len + 1) * sizeof(char));
