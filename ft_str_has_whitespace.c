e63fb1092ab4f7edb09bb19663d75905ab70b208
Nassr EL-Yamani <nassrel-yamani@Nassrs-MacBook-Pro.local>
2025-08-29 00:51:24 +0200

    adding more functions

---
+++ ft_str_has_whitespace.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_has_whitespace.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nel-yama <nel-yama@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 17:18:58 by nel-yama          #+#    #+#             */
+/*   Updated: 2025/04/27 17:18:58 by nel-yama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief : checks if any of the chars of a string is a white space.
+ * @param str : pointer to the string to browse.
+ * @return : 1 if a white space is found, 0 otherwise.
+ * space chars in ascii are as follow :
+ * Space (' ', ASCII 32)
+ * Tab ('\t', ASCII 9)
+ * Line feed ('\n', ASCII 10)
+ * Vertical tab ('\v', ASCII 11)
+ * Form feed ('\f', ASCII 12)
+ * Carriage return ('\r', ASCII 13)
+ */
+int	ft_str_has_whitespace(const char *str)
+{
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		if (ft_isspace(*str))
+			return (1);
+		str++;
+	}
+	return (0);
+}
